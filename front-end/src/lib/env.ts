399a0bac7f55823da004afde0778d0cf9dd0ad46
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-10-25 14:19:34 -0300

    feat : install ky

--- front-end/src/lib/env.ts
+++ front-end/src/lib/env.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod'
 const envSchema = z.object({
-  BACKEND_URL: z.string().url(),
+  BACKEND_API_URL: z.string().url().optional().default('http://localhost:3333'),
 })
 export const env = envSchema.parse(process.env)
