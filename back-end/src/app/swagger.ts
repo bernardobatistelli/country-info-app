3abd499f17c29c87d17a48abddb7a1bfa8de8ff5
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-10-25 11:19:36 -0300

    feat : project initial structure

---
+++ back-end/src/app/swagger.ts
@@ -0,0 +1,11 @@
+import swaggerAutogen from 'swagger-autogen'
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description',
+  },
+  host: 'localhost:3000',
+}
+const outputFile = './swagger-output.json'
+const routes = ['./src/http/controllers/routes.ts']
+swaggerAutogen()(outputFile, routes, doc)
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello from back-end!")
+import * as bodyParser from 'body-parser'
+import express from 'express'
+import { env } from './app/env'
+export const app = express()
+app.use(bodyParser.json())
+app.listen(env.PORT)
+// app.use('/api/v1/', routes)
+// app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+console.log(`Express server started on port ${env.PORT}`)
