30739fca18bac78d1c3d531ca60eda22bffa91f2
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-10-25 11:45:42 -0300

    feat : get available countries route

---
+++ back-end/src/http/controllers/country/country.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+
+import { GetAllAvailableCountriesController } from './get-available-countries.controller'
+
+const CountryRouter = Router({
+  mergeParams: true,
+})
+const getAllAvailableCountriesController =
+  new GetAllAvailableCountriesController()
+CountryRouter.get('/', (req, res) => {
+  getAllAvailableCountriesController.execute(req, res)
+})
+export default CountryRouter
